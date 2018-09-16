8b798e8c963c30ee6555c21d18e263b3b9d26348
Andrew Telyatko <steepashka1love@gmail.com>
2018-09-16 19:10:33 +0300

    Added CRUD base logic for user instance. Added a SQL script for creating lookup tables.

--- routes/users/validation-schemas/index.js
+++ routes/users/validation-schemas/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   CREATE_ONE: require('./create-one'),
   FETCH: require('./fetch'),
-  UPDATE_ONE: require('update-one'),
+  UPDATE_ONE: require('./update-one'),
   DELETE_ONE: require('./delete-one')
 };
