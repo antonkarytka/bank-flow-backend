ba8f119e616708fe38b2c4d68a87e40b9576827a
karytka <antnkrtk@gmail.com>
2018-10-15 21:02:13 +0300

    Add message to verify seeding has been successful, add error handling to seeder.

--- seeder/index.js
+++ seeder/index.js
@@ -5,4 +5,6 @@ return models.sequelize.sync({ force: true })
 .then(() => {
   const sql = fs.readFileSync(`${__dirname}/sql-scripts/create_lookups.sql`);
   return models.sequelize.query(sql.toString());
-});
+})
+.then(() => console.log('Successfully seeded the database!'))
+.catch(err => console.log(`Error occurred while seeding the databse: ${err}`));
