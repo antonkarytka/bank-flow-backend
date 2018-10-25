5df71d50ef1e42b9be18577876c04df067ab6cbe
karytka <antnkrtk@gmail.com>
2018-10-25 17:56:57 +0300

    Add an endpoint for fetching transitions.

--- models/transitions/methods/index.js
+++ models/transitions/methods/index.js
@@ -5,7 +5,7 @@ const { sequelize } = models;
 
 
 const createTransitionWithDependencies = (updateBankAccounts, content = {}, options = {}) => {
-  if (!updateBankAccounts) return Promise.reject('Method for manipulating bank accounts must be provided as first parameter');
+  if (!updateBankAccounts) return Promise.reject('Method for manipulating bank accounts must be provided as first parameter.');
 
   return sequelize.continueTransaction(options, transaction => {
     return Promise.resolve(updateBankAccounts(content, { ...options, transaction }))
